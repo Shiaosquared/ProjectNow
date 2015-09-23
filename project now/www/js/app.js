72d6b38e53d85880560d81f8b42e8768c2e686a5
Shiaosquared <slippers_chen@hotmail.com>
2015-09-23 10:40:46 -0700

    edited the descriptions in '$data'

--- project now/www/js/app.js
+++ project now/www/js/app.js
@@ -51,19 +51,18 @@
       data.items = [
           {
               title: 'Here Be Dragons: An Analysis of George Kennan’s Long Telegram',
-              desc: 'One prominent example of this tendency is the “Long Telegram” sent by George Kennan to Secretary',
-              user: '',
-              date: ''
+              desc: 'One prominent example of this tendency is the “Long Telegram” sent by George Kennan to Secretary of State James F. Brynes on February 22nd 1946...',
+              label: '3h'
           },
           {
-              title: 'Another Item Title',
+              title: 'To what extent can the failure of Pakistani democracy be attributed to Muhammad Ali Jinnah?',
               label: '6h',
-              desc: 'Ut enim ad minim veniam.'
+              desc: 'Since achieving independence from British India in 1947, Pakistan’s political landscape has been dominated by recurring cycles of political instability of political instability and military intervention... '
           },
           {
-              title: 'Yet Another Item Title',
+              title: 'Why Did "The US Policy of Containment" Fail in South Asia?',
               label: '1day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+              desc: 'Though South Asia was described by Lloyd Rudolph as being perpetually on the “backburner” of the United States’ foreign policy during the Cold War and so considered to be relatively less important than Western Europe or South-East Asia...'
           },
           {
               title: 'Yet Another Item Title',
